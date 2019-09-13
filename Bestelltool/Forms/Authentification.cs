96c5f225383360aaeb580f9ded697221c9d80721
Kleinrotti <c.rottmann@mathias-spital.de>
2019-09-13 13:03:29 +0200

    formatting

--- Bestelltool/Forms/Authentification.cs
+++ Bestelltool/Forms/Authentification.cs
@@ -8,149 +8,149 @@ using System.Windows.Forms;
 
 namespace Bestelltool
 {
-    public partial class Authentification : Form
-    {
-        public Authentification()
-        {
-            InitializeComponent();
-        }
+     public partial class Authentification : Form
+     {
+          public Authentification()
+          {
+               InitializeComponent();
+          }
 
-        #region Zur Bewegung des Fensters
+          #region Zur Bewegung des Fensters
 
-        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+          [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+          public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
-        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
-        public static extern bool ReleaseCapture();
+          [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+          public static extern bool ReleaseCapture();
 
-        public const int WM_NCLBUTTONDOWN = 0xA1;
-        public const int HT_CAPTION = 0x2;
+          public const int WM_NCLBUTTONDOWN = 0xA1;
+          public const int HT_CAPTION = 0x2;
 
-        #endregion Zur Bewegung des Fensters
+          #endregion Zur Bewegung des Fensters
 
-        private void LoadingScreen()
-        {
-            textBox_password.Visible = false;
-            textBox_username.Visible = false;
-            label_password.Visible = false;
-            label_username.Visible = false;
-            label_title.Visible = false;
+          private void LoadingScreen()
+          {
+               textBox_password.Visible = false;
+               textBox_username.Visible = false;
+               label_password.Visible = false;
+               label_username.Visible = false;
+               label_title.Visible = false;
 
-            button_enter.Visible = false;
-            pictureBox_loading.Location = new System.Drawing.Point(163, 161);
-            pictureBox_loading.Visible = true;
-            label_loading.Visible = true;
-        }
+               button_enter.Visible = false;
+               pictureBox_loading.Location = new System.Drawing.Point(163, 161);
+               pictureBox_loading.Visible = true;
+               label_loading.Visible = true;
+          }
 
-        private void InputScreen()
-        {
-            pictureBox_loading.Visible = false;
-            textBox_password.Visible = true;
-            textBox_username.Visible = true;
-            label_password.Visible = true;
-            label_username.Visible = true;
-            label_title.Visible = true;
-            label_loading.Visible = false;
-            button_enter.Visible = true;
-            textBox_username.Text = Environment.UserName;
-            this.SelectNextControl(textBox_username, true, false, false, false);
-        }
+          private void InputScreen()
+          {
+               pictureBox_loading.Visible = false;
+               textBox_password.Visible = true;
+               textBox_username.Visible = true;
+               label_password.Visible = true;
+               label_username.Visible = true;
+               label_title.Visible = true;
+               label_loading.Visible = false;
+               button_enter.Visible = true;
+               textBox_username.Text = Environment.UserName;
+               this.SelectNextControl(textBox_username, true, false, false, false);
+          }
 
-        private async void button_enter_Click(object sender, EventArgs e)
-        {
-            Credentials cred = new Credentials
-            {
-                Username = textBox_username.Text,
-                Domain = Settings.Default.WindowsDomain,
-            };
-            SecureStringHandler.StringToSecureString(ref cred.Password, textBox_password.Text);
+          private async void button_enter_Click(object sender, EventArgs e)
+          {
+               Credentials cred = new Credentials
+               {
+                    Username = textBox_username.Text,
+                    Domain = Settings.Default.WindowsDomain,
+               };
+               SecureStringHandler.StringToSecureString(ref cred.Password, textBox_password.Text);
 
-            textBox_password.Text = "";
-            DomainAuthentification d = new DomainAuthentification(this.Controls);
+               textBox_password.Text = "";
+               DomainAuthentification d = new DomainAuthentification(this.Controls);
 
-            LoadingScreen();
+               LoadingScreen();
 
-            await Task.Factory.StartNew(() =>
-            {
-                d.IsValid(cred);
-            });
-            if (d.Valid)
-            {
-                await d.PasswordInRegistry(cred);
-                SqlOperations sql = new SqlOperations(new SqlConnectionStringBuilder()
-            { DataSource = "127.0.0.1\\BESTELLSOFTWARE", IntegratedSecurity = true, InitialCatalog = "bestellsoftware" }.ToString());
-            MessageBox.Show(sql.Authenticate.ToString());
-                Hide();
-                UI u = new UI(cred);
-                u.ShowDialog();
-                Close();
-            }
-            else
-            {
-                InputScreen();
-                MessageBox.Show(Lang.GetText("message_authentification_wrongcredentials"));
-            }
-        }
-
-        private void Authentification_Load(object sender, EventArgs e)
-        {
-            Configuration c = new Configuration();
-            c.SetStandartPaths();
-            Lang lang = new Lang();
-            Lang.LanguageChanged += UpdateLangUI;
-            LoadTranslation();
-            InputScreen();
-        }
-
-        private void UpdateLangUI(object sender, EventArgs e)
-        {
-            LoadTranslation();
-        }
-
-        private void LoadTranslation()
-        {
-            label_loading.Text = Lang.GetText("control_authentification_label_loading");
-            label_password.Text = Lang.GetText("control_authentification_label_password");
-            label_username.Text = Lang.GetText("control_authentification_label_username");
-            label_title.Text = Lang.GetText("control_authentification_label_title");
-            button_enter.Text = Lang.GetText("control_authentification_button_enter");
-        }
-
-        private void Authentification_KeyDown(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyValue)
-            {
-                case (char)Keys.Enter:
-                    button_enter.PerformClick();
-                    break;
-
-                case (char)Keys.Escape:
+               await Task.Factory.StartNew(() =>
+               {
+                    d.IsValid(cred);
+               });
+               if (d.Valid)
+               {
+                    await d.PasswordInRegistry(cred);
+                    SqlOperations sql = new SqlOperations(new SqlConnectionStringBuilder()
+                    { DataSource = "127.0.0.1\\BESTELLSOFTWARE", IntegratedSecurity = true, InitialCatalog = "bestellsoftware" }.ToString());
+                    MessageBox.Show(sql.Authenticate.ToString());
+                    Hide();
+                    UI u = new UI(cred);
+                    u.ShowDialog();
                     Close();
-                    break;
-            }
-        }
+               }
+               else
+               {
+                    InputScreen();
+                    MessageBox.Show(Lang.GetText("message_authentification_wrongcredentials"));
+               }
+          }
 
-        private void Authentification_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Environment.Exit(0);
-        }
+          private void Authentification_Load(object sender, EventArgs e)
+          {
+               Configuration c = new Configuration();
+               c.SetStandartPaths();
+               Lang lang = new Lang();
+               Lang.LanguageChanged += UpdateLangUI;
+               LoadTranslation();
+               InputScreen();
+          }
 
-        private void panel_top_Paint(object sender, PaintEventArgs e)
-        {
-        }
+          private void UpdateLangUI(object sender, EventArgs e)
+          {
+               LoadTranslation();
+          }
 
-        private void panel_top_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
-            }
-        }
+          private void LoadTranslation()
+          {
+               label_loading.Text = Lang.GetText("control_authentification_label_loading");
+               label_password.Text = Lang.GetText("control_authentification_label_password");
+               label_username.Text = Lang.GetText("control_authentification_label_username");
+               label_title.Text = Lang.GetText("control_authentification_label_title");
+               button_enter.Text = Lang.GetText("control_authentification_button_enter");
+          }
 
-        private void button_exit_Click(object sender, EventArgs e)
-        {
-            Environment.Exit(0);
-        }
-    }
+          private void Authentification_KeyDown(object sender, KeyEventArgs e)
+          {
+               switch (e.KeyValue)
+               {
+                    case (char)Keys.Enter:
+                         button_enter.PerformClick();
+                         break;
+
+                    case (char)Keys.Escape:
+                         Close();
+                         break;
+               }
+          }
+
+          private void Authentification_FormClosing(object sender, FormClosingEventArgs e)
+          {
+               Environment.Exit(0);
+          }
+
+          private void panel_top_Paint(object sender, PaintEventArgs e)
+          {
+          }
+
+          private void panel_top_MouseDown(object sender, MouseEventArgs e)
+          {
+               if (e.Button == MouseButtons.Left)
+               {
+                    ReleaseCapture();
+                    SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+               }
+          }
+
+          private void button_exit_Click(object sender, EventArgs e)
+          {
+               Environment.Exit(0);
+          }
+     }
 }
--- Bestelltool/UserControl/CompletedOrders.cs
+++ Bestelltool/UserControl/CompletedOrders.cs
@@ -10,111 +10,111 @@ using System.Windows.Forms;
 
 namespace Bestelltool
 {
-    public partial class CompletedOrders : UserControl
-    {
-        private readonly DataTable _dt;
-        private Orders _b;
-        private DataTableCache _memorycache;
+     public partial class CompletedOrders : UserControl
+     {
+          private readonly DataTable _dt;
+          private Orders _b;
+          private DataTableCache _memorycache;
 
-        public CompletedOrders()
-        {
-            InitializeComponent();
-            button_search.Visible = true;
-            button_update.Visible = true;
-            _dt = new DataTable();
-            this._dt.Clear();
-            this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_id"), typeof(int));
-            this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_product"));
-            this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_customer"));
-            this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_costcentre"));
-            this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_amount"), typeof(int));
-            this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_date"), typeof(DateTime));
-            button_search.Text = Lang.GetText("control_completedorders_button_search");
-            button_update.Text = Lang.GetText("control_completedorders_dt_column_button_update");
-            this.dataGridView1.DataSource = this._dt;
-            dataGridView1.Columns[0].Width = 50;
-            dataGridView1.Columns[1].Width = 380;
-            dataGridView1.Columns[2].Width = 110;
-            dataGridView1.Columns[3].Width = 240;
-            dataGridView1.Columns[4].Width = 50;
-            dataGridView1.Columns[5].Width = 140;
-            progressBar_loaded.Visible = false;
-            this.dataGridView1.CellValueNeeded += new
-            DataGridViewCellValueEventHandler(dataGridView1_CellValueNeeded);
-        }
+          public CompletedOrders()
+          {
+               InitializeComponent();
+               button_search.Visible = true;
+               button_update.Visible = true;
+               _dt = new DataTable();
+               this._dt.Clear();
+               this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_id"), typeof(int));
+               this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_product"));
+               this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_customer"));
+               this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_costcentre"));
+               this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_amount"), typeof(int));
+               this._dt.Columns.Add(Lang.GetText("control_completedorders_dt_column_date"), typeof(DateTime));
+               button_search.Text = Lang.GetText("control_completedorders_button_search");
+               button_update.Text = Lang.GetText("control_completedorders_dt_column_button_update");
+               this.dataGridView1.DataSource = this._dt;
+               dataGridView1.Columns[0].Width = 50;
+               dataGridView1.Columns[1].Width = 380;
+               dataGridView1.Columns[2].Width = 110;
+               dataGridView1.Columns[3].Width = 240;
+               dataGridView1.Columns[4].Width = 50;
+               dataGridView1.Columns[5].Width = 140;
+               progressBar_loaded.Visible = false;
+               this.dataGridView1.CellValueNeeded += new
+               DataGridViewCellValueEventHandler(dataGridView1_CellValueNeeded);
+          }
 
-        private void dataGridView1_CellValueNeeded(object sender,
-        DataGridViewCellValueEventArgs e)
-        {
-            e.Value = _memorycache.RetrieveElement(e.RowIndex, e.ColumnIndex);
-        }
+          private void dataGridView1_CellValueNeeded(object sender,
+          DataGridViewCellValueEventArgs e)
+          {
+               e.Value = _memorycache.RetrieveElement(e.RowIndex, e.ColumnIndex);
+          }
 
-        private void CompletedOrders_Load(object sender, EventArgs e)
-        {
-            //UpdateList();
-            dataGridView1.VirtualMode = true;
-            //_memorycache = new DataTableCache(sql, 16);
-            //foreach (DataColumn column in sql.Columns)
-            //{
-            //    dataGridView1.Columns.Add(column.ColumnName, column.ColumnName);
-            //}
-            //dataGridView1.RowCount = sql.RowCount;
-        }
+          private void CompletedOrders_Load(object sender, EventArgs e)
+          {
+               //UpdateList();
+               dataGridView1.VirtualMode = true;
+               //_memorycache = new DataTableCache(sql, 16);
+               //foreach (DataColumn column in sql.Columns)
+               //{
+               //    dataGridView1.Columns.Add(column.ColumnName, column.ColumnName);
+               //}
+               //dataGridView1.RowCount = sql.RowCount;
+          }
 
-        private async void UpdateList()
-        {
-            progressBar_loaded.Visible = true;
-            _dt.Clear();
-            progressBar_loaded.Value = 0;
-            _b = new Orders();
-            _b.GetOrders();
-            progressBar_loaded.Maximum = _b.Bestellblatt.Count;
-            await Task.Factory.StartNew(() =>
-            {
-                Parallel.ForEach(_b.Bestellblatt, (v) =>
-                {
-                    Invoke(new MethodInvoker(() => AddToDataTable(v)));
-                });
-            });
-            progressBar_loaded.Visible = false;
-        }
+          private async void UpdateList()
+          {
+               progressBar_loaded.Visible = true;
+               _dt.Clear();
+               progressBar_loaded.Value = 0;
+               _b = new Orders();
+               _b.GetOrders();
+               progressBar_loaded.Maximum = _b.Bestellblatt.Count;
+               await Task.Factory.StartNew(() =>
+               {
+                    Parallel.ForEach(_b.Bestellblatt, (v) =>
+                 {
+                        Invoke(new MethodInvoker(() => AddToDataTable(v)));
+                   });
+               });
+               progressBar_loaded.Visible = false;
+          }
 
-        private void button_update_Click(object sender, EventArgs e)
-        {
-            UpdateList();
-        }
+          private void button_update_Click(object sender, EventArgs e)
+          {
+               UpdateList();
+          }
 
-        private void AddToDataTable(Order b)          //Ein Item in eine neue DataTable Zeile einfügen
-        {
-            DataRow row = _dt.NewRow();
-            row[Lang.GetText("control_completedorders_dt_column_id")] = b.Number;
-            row[Lang.GetText("control_completedorders_dt_column_product")] = b.Product;
-            row[Lang.GetText("control_completedorders_dt_column_customer")] = b.Buyer;
-            row[Lang.GetText("control_completedorders_dt_column_costcentre")] = b.Costcentre;
-            row[Lang.GetText("control_completedorders_dt_column_amount")] = b.Ammount;
-            row[Lang.GetText("control_completedorders_dt_column_date")] = b.Date;
-            _dt.Rows.Add(row);
-            progressBar_loaded.PerformStep();
-        }
+          private void AddToDataTable(Order b)          //Ein Item in eine neue DataTable Zeile einfügen
+          {
+               DataRow row = _dt.NewRow();
+               row[Lang.GetText("control_completedorders_dt_column_id")] = b.Number;
+               row[Lang.GetText("control_completedorders_dt_column_product")] = b.Product;
+               row[Lang.GetText("control_completedorders_dt_column_customer")] = b.Buyer;
+               row[Lang.GetText("control_completedorders_dt_column_costcentre")] = b.Costcentre;
+               row[Lang.GetText("control_completedorders_dt_column_amount")] = b.Ammount;
+               row[Lang.GetText("control_completedorders_dt_column_date")] = b.Date;
+               _dt.Rows.Add(row);
+               progressBar_loaded.PerformStep();
+          }
 
-        private void button_search_Click(object sender, EventArgs e)
-        {
-            var search = Interaction.InputBox(Lang.GetText("control_completedorders_search"));
-            if (search != string.Empty && search.Length > 0)
-            {
-                _dt.Clear();
-                progressBar_loaded.Visible = true;
-                foreach (Order v in _b.Bestellblatt)
-                {
-                    if (v.Buyer.ToLower()
-                            .Contains(search.ToLower()) || v.Costcentre.ToLower().Contains(search.ToLower()) ||
-                                v.Product.ToLower().Contains(search.ToLower()))
+          private void button_search_Click(object sender, EventArgs e)
+          {
+               var search = Interaction.InputBox(Lang.GetText("control_completedorders_search"));
+               if (search != string.Empty && search.Length > 0)
+               {
+                    _dt.Clear();
+                    progressBar_loaded.Visible = true;
+                    foreach (Order v in _b.Bestellblatt)
                     {
-                        AddToDataTable(v);
+                         if (v.Buyer.ToLower()
+                                 .Contains(search.ToLower()) || v.Costcentre.ToLower().Contains(search.ToLower()) ||
+                                     v.Product.ToLower().Contains(search.ToLower()))
+                         {
+                              AddToDataTable(v);
+                         }
                     }
-                }
-                progressBar_loaded.Visible = false;
-            }
-        }
-    }
+                    progressBar_loaded.Visible = false;
+               }
+          }
+     }
 }
